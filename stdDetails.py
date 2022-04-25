ff781eb0bb292dd67237383ffebef1b974dd9591
sanjay03 <sanjay69102@gmail.com>
2022-04-26 00:34:00 +0530

    first commit

---
+++ stdDetails.py
@@ -0,0 +1,8 @@
+def std_Details():
+    name = input("ENTER YOUR NAME : ")
+    id = input ("ENTER YOUR ID : ")
+    gender = input ("ETER YOUR GENDER : ")
+    print(name)
+    print(id)
+    print(gender)
+std_Details()
